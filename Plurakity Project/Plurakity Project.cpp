2da657ade308dc5a97a762f5948bd1bbc4d6838b
IMDimov18 <IMDimov18@codingburgas.bg>
2020-10-24 19:59:47 +0300

    Add functions and finish the users input function

--- Plurakity Project/Plurakity Project.cpp
+++ Plurakity Project/Plurakity Project.cpp
@@ -1,6 +1,56 @@
 #include <iostream>
+using namespace std;
+
+float set1[100];
+float set2[100];
+
+float userInput()
+{
+    int n,n1;
+    cin >> n;
+    cin >> n1;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> set1[i];
+    }
+    for (int i = 0; i < n1; i++)
+    {
+        cin >> set2[i];
+    }
+    
+    return *set1;
+    return *set2;
+}
+
+float unitingSets()
+{
+    int n = sizeof(set1) / sizeof(set1[0]);
+    int m = sizeof(set2) / sizeof(set2[0]);
+    int k = n + m;
+
+}
+
+float subsets()
+{
+
+}
+
+float ownSubsets()
+{
+
+}
+
+float equivalentSubsets()
+{
+
+}
+
+float essentialSubsets()
+{
+
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    userInput(); 
 }
