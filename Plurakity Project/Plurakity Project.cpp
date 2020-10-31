2e7ee40fe45b900b73c6fddd8f4618837360c008
IMDimov18 <IMDimov18@codingburgas.bg>
2020-10-31 19:15:48 +0200

    Finish the subsets function

--- Plurakity Project/Plurakity Project.cpp
+++ Plurakity Project/Plurakity Project.cpp
@@ -3,10 +3,12 @@ using namespace std;
 
 float set1[100];
 float set2[100];
+int n = 0;
+int n1 = 0;
 
 float userInput()
 {
-    int n,n1;
+   
     cin >> n;
     cin >> n1;
     for (int i = 0; i < n; i++)
@@ -20,6 +22,8 @@ float userInput()
     
     return *set1;
     return *set2;
+    return n;
+    return n1;
 }
 
 void unitingSets()
@@ -47,7 +51,9 @@ void unitingSets()
 
 void subsets()
 {
-    
+    int count = 0,i=0;
+    int* ptr = &count;
+    int* ptr1 = &i;
     if (sizeof(set1) > sizeof(set2))
     {
         int set3(sizeof(set2));
@@ -57,17 +63,48 @@ void subsets()
             {
                 if (set1[i] == set2[j])
                 {
-                    set3[i] = set2[j];
+                    set3[ptr++] = set2[j];
                 }
             }
+        }
+        for (i=0; i < count; i++)
+        {
+            cout << set3[ptr] << " ";
+        }
+    }
+    else
+    {
+        int set3(sizeof(set2));
+        for (int i = 0; i < sizeof(set2); i++)
+        {
+            for (int j = 0; j < sizeof(set1); j++)
+            {
+                if (set2[i] == set1[j])
+                {
+                    set3[ptr++] = set1[j];
+                }
+            }
+        }
+        for (int i = 0; i < count; i++)
+        {
+            cout << set3[ptr] <<" ";
         }
     }
 }
 
+void section()
+{
+
+}
+
+void distinction()
+{
+
+}
+
 int main()
 {
-    float set1[100];
-    float set2[100];
-    userInput(); 
-    unitingSets();
+    userInput();
+    subsets();
+ 
 }
